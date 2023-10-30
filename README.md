4750fc751a5a9e70fadc08cbba898bc782ae44e5
sskk <susukkekki@gmail.com>
2023-10-30 21:32:07 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# K8s Java Client Example
+
+> K8s Java Client 예제로 Custom Resource 를 조회하고 이를 테스트하는 예제코드이다.
+>
+> - https://github.com/kubernetes-client/java/wiki/3.-Code-Examples
+> - https://developers.redhat.com/articles/2023/01/24/how-write-tests-fabric8-kubernetes-client#how_to_write_tests_using_fabric8_kubernetes_mock_server
+
+- [K8s Java Client Example](#k8s-java-client-example)
--- src/test/java/io/sskk/kubernetes/crd/CustomResourceTest.java
+++ src/test/java/io/sskk/kubernetes/crd/CustomResourceTest.java
@@ -1,10 +1,31 @@
 package io.sskk.kubernetes.crd;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+
+import io.fabric8.junit.jupiter.api.KubernetesTest;
+import io.fabric8.junit.jupiter.api.RequireK8sSupport;
+import io.fabric8.junit.jupiter.api.RequireK8sVersionAtLeast;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+@KubernetesTest
+@RequireK8sSupport(Pod.class)
+@RequireK8sVersionAtLeast(majorVersion = 1, minorVersion = 16)
 public class CustomResourceTest {
+    KubernetesClient kubernetesClient;
+
     @Test
     public void testGetApiVersion() {
+        // // Given
+        // Map<String, String> matchLabel = Collections.singletonMap("app", "list");
+        // PodGroupService podGroupService = new PodGroupService(kubernetesClient, matchLabel);
+        // // When
+        // PodList podList = podGroupService.list();
 
+        // // Then
+        // assertNotNull(podList);
     }
 }
