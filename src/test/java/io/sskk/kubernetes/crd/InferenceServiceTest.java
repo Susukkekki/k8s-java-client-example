3b225c1b5cf22ec2e6996b20f3c2280075eb6eb6
sskk <susukkekki@gmail.com>
2023-10-31 23:06:34 +0900

    add patch and delete

--- src/test/java/io/sskk/kubernetes/crd/InferenceServiceTest.java
+++ src/test/java/io/sskk/kubernetes/crd/InferenceServiceTest.java
@@ -289,4 +289,130 @@ public class InferenceServiceTest {
             "{a=b, kubectl.kubernetes.io/last-applied-configuration={"apiVersion":"serving.kserve.io/v1beta1","kind":"InferenceService","metadata":{"annotations":{},"name":"sklearn-iris","namespace":"default"},"spec":{"predictor":{"model":{"modelFormat":{"name":"sklearn"},"storageUri":"gs://kfserving-examples/models/sklearn/1.0/model"}}}}    
          */
     }
+
+    /**
+     * @see https://github.com/fabric8io/kubernetes-client/blob/main/doc/CHEATSHEET.md#resource-typed-api
+     */
+    @Test
+    public void testPatchTypedInferenceService() {
+        KubernetesClient client = new KubernetesClientBuilder().build();
+        MixedOperation<InferenceService, KubernetesResourceList<InferenceService>, Resource<InferenceService>> inferenceServiceClient = client.resources(InferenceService.class);
+
+        String name = "test";
+        String storageUri = "gs://kfserving-examples/models/sklearn/1.0/model2";
+
+        InferenceService inferenceService = new InferenceService();
+        InferenceServiceSpec inferenceServiceSpec = new InferenceServiceSpec();
+        inferenceService.getMetadata().setName(name);
+        inferenceService.setSpec(inferenceServiceSpec);
+
+        Predictor predictor = new Predictor();
+        Model model = new Model();
+        // ModelFormat modelFormat = new ModelFormat();
+        // modelFormat.setName("sklearn");
+        model.setStorageUri(storageUri);
+        // model.setModelFormat(modelFormat);
+        predictor.setModel(model);
+        inferenceService.getSpec().setPredictor(predictor);        
+
+        InferenceService createdInferenceService = inferenceServiceClient.inNamespace("default").resource(inferenceService).patch();
+        assertEquals(name, createdInferenceService.getMetadata().getName());
+        assertEquals(storageUri, createdInferenceService.getSpec().getPredictor().getModel().getStorageUri());
+
+        // KubernetesResourceList<InferenceService> list = inferenceServiceClient.inNamespace("default").list();
+        // assertTrue(list.getItems().size() >= 2);
+        // assertEquals("storageUri", list.getItems().get(0).getSpec().getPredictor().getModel().getModelFormat().getName());
+    }
+
+    /**
+     * @see https://github.com/fabric8io/kubernetes-client/blob/main/doc/CHEATSHEET.md#resource-typeless-api
+     */
+    @Test
+    public void testPathTypessInferenceServiceWithYaml() {
+        ResourceDefinitionContext resourceDefinitionContext = new ResourceDefinitionContext.Builder()
+            .withVersion(VERSION)
+            .withGroup(GROUP)
+            .withPlural("inferenceservices")
+            .withNamespaced(true)
+            .build();
+
+        KubernetesClient client = new KubernetesClientBuilder().build();
+
+        String name = "test";
+        String storageUri = "gs://kfserving-examples/models/sklearn/1.0/model3";
+
+        String rawYaml = ""
+                          .concat("apiVersion: \"serving.kserve.io/v1beta1\"\n")
+                          .concat("kind: \"InferenceService\"\n")
+                          .concat("metadata:\n")
+            .concat(String.format("  name: \"%s\"\n", name))
+                          .concat("spec:\n")
+                          .concat("  predictor:\n")
+                          .concat("    model:\n")
+                          .concat("      modelFormat:\n")
+                          .concat("        name: sklearn\n")
+            .concat(String.format("      storageUri: \"%s\"", storageUri));        
+        
+        GenericKubernetesResource object = client.genericKubernetesResources(resourceDefinitionContext)
+            .inNamespace("default")    
+            .load(new ByteArrayInputStream(rawYaml.getBytes())).patch();        
+
+        Map<String, Object> spec = (Map<String, Object>)object.getAdditionalProperties().get("spec");
+        Map<String, Object> predictor = (Map<String, Object>)spec.get("predictor");
+        Map<String, Object> model = (Map<String, Object>)predictor.get("model");
+        String returnedStorageUri = (String)model.get("storageUri");
+        Map<String, Object> modelFormat = (Map<String, Object>)model.get("modelFormat");
+        String modelFormatName = (String)modelFormat.get("name");
+
+        assertEquals(name, object.getMetadata().getName());
+        assertEquals(storageUri, returnedStorageUri);
+
+        /*        
+        list.getItems().get(0).getMetadata().getAnnotations().toString()        
+            "{a=b, kubectl.kubernetes.io/last-applied-configuration={"apiVersion":"serving.kserve.io/v1beta1","kind":"InferenceService","metadata":{"annotations":{},"name":"sklearn-iris","namespace":"default"},"spec":{"predictor":{"model":{"modelFormat":{"name":"sklearn"},"storageUri":"gs://kfserving-examples/models/sklearn/1.0/model"}}}}    
+         */
+    }
+
+    /**
+     * @see https://github.com/fabric8io/kubernetes-client/blob/main/doc/CHEATSHEET.md#resource-typed-api
+     */
+    @Test
+    public void testDeleteTypedInferenceService() {
+        KubernetesClient client = new KubernetesClientBuilder().build();
+        MixedOperation<InferenceService, KubernetesResourceList<InferenceService>, Resource<InferenceService>> inferenceServiceClient = client.resources(InferenceService.class);
+
+        String name = "test";
+
+        InferenceService inferenceService = new InferenceService();
+        inferenceService.getMetadata().setName(name);        
+
+        inferenceServiceClient.inNamespace("default").resource(inferenceService).delete();                
+    }
+
+    /**
+     * @see https://github.com/fabric8io/kubernetes-client/blob/main/doc/CHEATSHEET.md#resource-typeless-api
+     */
+    @Test
+    public void testDeleteTypessInferenceServiceWithYaml() {
+        ResourceDefinitionContext resourceDefinitionContext = new ResourceDefinitionContext.Builder()
+            .withVersion(VERSION)
+            .withGroup(GROUP)
+            .withPlural("inferenceservices")
+            .withNamespaced(true)
+            .build();
+
+        KubernetesClient client = new KubernetesClientBuilder().build();
+
+        String name = "test-2";
+
+        String rawYaml = ""
+                          .concat("apiVersion: \"serving.kserve.io/v1beta1\"\n")
+                          .concat("kind: \"InferenceService\"\n")
+                          .concat("metadata:\n")
+            .concat(String.format("  name: \"%s\"\n", name));            
+        
+        client.genericKubernetesResources(resourceDefinitionContext)
+            .inNamespace("default")    
+            .load(new ByteArrayInputStream(rawYaml.getBytes())).delete();        
+    }
 }
